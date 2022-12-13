8f82790f7c4f7be27817efc1ccfbe0c9826427b7
gaganrider <114590365+gaganrider@users.noreply.github.com>
2022-12-13 18:06:30 +0530

    Updates

---
+++ precache-manifest.5908369c5291074939046a0868557f83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "403ad02708c3e7a45045",
+    "url": "/reactapp/static/css/main.d7517f79.chunk.css"
+  },
+  {
+    "revision": "403ad02708c3e7a45045",
+    "url": "/reactapp/static/js/main.403ad027.chunk.js"
+  },
+  {
+    "revision": "87da9049baad9346c4c2",
+    "url": "/reactapp/static/js/1.87da9049.chunk.js"
+  },
+  {
+    "revision": "75024734ab9c32c58e65",
+    "url": "/reactapp/static/js/runtime~main.75024734.js"
+  },
+  {
+    "revision": "5e9d3abc528726a7bbc85b98b0231167",
+    "url": "/reactapp/index.html"
+  }
+];
